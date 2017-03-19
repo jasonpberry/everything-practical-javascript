664200378ae351fefa50f678e7c19b54077a44f5
Jason Berry <jason.berry@boxwoodtech.com>
2017-03-19 11:09:41 -0400

    nocase: removing store call from render method

--- Beasts/Challenge-2/js/app.js
+++ Beasts/Challenge-2/js/app.js
@@ -70,7 +70,6 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -92,6 +91,7 @@ jQuery(function ($) {
 			});
 
 			this.render();
+			util.store('todos-jquery', this.todos);
 		},
 		getActiveTodos: function () {
 			return this.todos.filter(function (todo) {
@@ -118,6 +118,7 @@ jQuery(function ($) {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
 			this.render();
+			util.store('todos-jquery', this.todos);
 		},
 		// accepts an element from inside the `.item` div and
 		// returns the corresponding index in the `todos` array
@@ -149,11 +150,13 @@ jQuery(function ($) {
 			$input.val('');
 
 			this.render();
+			util.store('todos-jquery', this.todos);
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
 			this.render();
+			util.store('todos-jquery', this.todos);
 		},
 		edit: function (e) {
 			var $input = $(e.target).closest('li').addClass('editing').find('.edit');
@@ -185,10 +188,12 @@ jQuery(function ($) {
 			}
 
 			this.render();
+			util.store('todos-jquery', this.todos);			
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
 			this.render();
+			util.store('todos-jquery', this.todos);
 		}
 	};
 
