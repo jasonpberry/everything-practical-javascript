09a1377c27ea3057d03bf70f5c07f04b7e2ea544
Jason Berry <jason.berry@boxwoodtech.com>
2017-03-19 11:38:15 -0400

    nocase: challeng 4 added.  Moved my todo app to build from scratch folder

---
+++ Beasts/Challenge-4/librarySystemWithDependencies.js
@@ -0,0 +1,62 @@
+(function() {
+    var libraryStorage = {};
+
+    function librarySystem(libraryName, dependencies, callback) {
+        if (arguments.length > 1) {
+
+            libraryStorage[libraryName] = callback();
+  
+            if(dependencies.length > 0) {
+
+               var librariesToAdd = [];
+               // Loop over dependencies. 
+               for(var i = 0; i < dependencies.length; i++) {
+                  libraryToAdd = dependencies[i];
+                  loadedLibrary = librarySystem(libraryToAdd);
+                  librariesToAdd[i] = loadedLibrary;
+               }
+               libraryStorage[libraryName] = callback.apply(null, librariesToAdd);
+
+            } else {
+               libraryStorage[libraryName] = callback();
+            }
+        } else {
+            return libraryStorage[libraryName];
+        } 
+    }
+    window.librarySystem = librarySystem;
+})();
+
+librarySystem('name', [], function() {
+  return 'Gordon';
+});
+
+librarySystem('company', [], function() {
+  return 'Watch and Code';
+});
+
+librarySystem('workBlurb', ['name', 'company'], function(name, company) {
+  return name + ' works at ' + company;
+});
+
+var workBlurb = librarySystem('workBlurb'); // 'Gordon works at Watch and Code'
+
+console.log(workBlurb);
+
+
+librarySystem('dependency', [], function() {
+  return 'loaded dependency';
+});
+
+librarySystem('another', [], function() {
+  return 'another dependency';
+});
+
+librarySystem('app', ['dependency', 'another'], function(dependency, another) {
+  return 'app with ' + dependency + ' + ' + another;
+});
+
+var app = librarySystem('app'); // 'app with loaded dependency'
+
+console.log(app);
+
